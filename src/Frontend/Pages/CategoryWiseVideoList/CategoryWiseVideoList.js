01bebde4c1588af4bf587eb5e35213da0b145cfa
kashifhussainpathan <121045014+kashifhussainpathan@users.noreply.github.com>
2023-07-31 15:01:41 +0530

    Refactored

--- src/Frontend/Pages/CategoryWiseVideoList/CategoryWiseVideoList.js
+++ src/Frontend/Pages/CategoryWiseVideoList/CategoryWiseVideoList.js
@@ -23,25 +23,6 @@ export const CategoryWiseVideoList = () => {
       <div className="videos-wrapper">
         {categoryWiseVideoList?.map((video) => (
           <VideoCard {...video} key={video._id} />
-          // <div key={_id} className="video-wrapper">
-          //   <div className="video-image">
-          //     <img src={thumbnail} alt={title} />
-          //     <span onClick={() => addToWatchList(_id)}>
-          //       {" "}
-          //       <MdOutlineWatchLater />
-          //     </span>
-          //   </div>
-          //   <div className="video-details">
-          //     <div className="dp-and-title">
-          //       <img src="https://picsum.photos/40/40" alt="dp" />
-          //       <h4> {title}</h4>
-          //     </div>
-
-          //     <div className="views-creator">
-          //       {views} | {creator}
-          //     </div>
-          //   </div>
-          // </div>
         ))}
       </div>
     </div>
